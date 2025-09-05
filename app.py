619c7c3d698afa05109b07c15d6dc2c21f92303b
ravi000111 <ravis541242@gmail.com>
2025-09-05 21:14:46 +0530

    app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ whisper_model = WhisperModel(WHISPER_SIZE, device=DEVICE)
 llm_client = openai.OpenAI(
     base_url="https://api.groq.com/openai/v1",
     model = "llama-3.1-8b-instant",
-    api_key=os.environ.get("openai_api")
+    api_key=os.environ.get("OPENAI_API")
 )
 
 # ──────────────── HELPERS ────────────────
